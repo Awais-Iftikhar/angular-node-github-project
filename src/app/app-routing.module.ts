186fe9ff607bc393d38546456b3a8d1352a3c157
Awais-Iftikhar <awaissunny00@gmail.com>
2020-08-22 19:57:13 +0500

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { ShowusersComponent } from './user/showusers/showusers.component';
+import { CreateuserComponent } from './user/createuser/createuser.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: CreateuserComponent},
+  {path: 'users' , component: ShowusersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
